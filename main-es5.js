f4c568e809f928b6e397bf17ee496329768196d8
Amrik Singh <amriksg81@gmail.com>
2020-07-08 19:15:31 -0700

    API URL Updated

--- main-es5.js
+++ main-es5.js
@@ -2729,11 +2729,11 @@ function _classCallCheck(instance, Constructor) { if (!(instance instanceof Cons
       // CurrentWebsite - Pass Enum Text from Below WEBSITES Enum
       // CurrentWebsiteID - Pass Enum ID from Below WEBSITES Enum
       // Load DoorBC Website
-      CurrentWebsite: "DoorsBC",
-      CurrentWebsiteID: 4 // Load Doors Seattle Website
-      //CurrentWebsite: "DoorSeattle",
-      //CurrentWebsiteID: 1
-
+      //CurrentWebsite: "DoorsBC",
+      //CurrentWebsiteID: 4
+      // Load Doors Seattle Website
+      CurrentWebsite: "DoorSeattle",
+      CurrentWebsiteID: 1
     };
     /* Setting Different Websites Values depending on the Website Domain to load content of the selected website */
 
