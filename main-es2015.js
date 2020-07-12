f9cfa22ba8f5bdb834a6d9b94942f3984131c143
Amrik Singh <amriksg81@gmail.com>
2020-07-12 14:21:08 -0700

    new changes added

--- main-es2015.js
+++ main-es2015.js
@@ -1408,11 +1408,11 @@ const environment = {
     // CurrentWebsite - Pass Enum Text from Below WEBSITES Enum
     // CurrentWebsiteID - Pass Enum ID from Below WEBSITES Enum
     // Load DoorBC Website
-    //CurrentWebsite: "DoorsBC",
-    //CurrentWebsiteID: 4
+    CurrentWebsite: "DoorsBC",
+    CurrentWebsiteID: 4
     // Load Doors Seattle Website
-    CurrentWebsite: "DoorSeattle",
-    CurrentWebsiteID: 1
+    //CurrentWebsite: "DoorSeattle",
+    //CurrentWebsiteID: 1
 };
 /* Setting Different Websites Values depending on the Website Domain to load content of the selected website */
 var WEBSITES;
